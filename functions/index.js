d63365d5cda52514804dc203659032a8c2e3edc7
oscarrc <oscarrc.web@gmail.com>
2020-07-09 20:06:16 +0200

    [MOD] Change function region

--- functions/index.js
+++ functions/index.js
@@ -19,43 +19,45 @@ let transporter = nodemailer.createTransport({
   }
 });
 
-exports.sendMail = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const from = req.body.from;
-    const subject = req.body.subject;
-    const message = req.body.message;
+exports.sendMail = functions
+  .region("europe-west3")
+  .https.onRequest((req, res) => {
+    cors(req, res, () => {
+      const from = req.body.from;
+      const subject = req.body.subject;
+      const message = req.body.message;
 
-    const sentMail = {
-      from: process.env.NAME + " " + process.env.USER,
-      to: process.env.NAME + " " + process.env.USER,
-      subject: subject,
-      html: `<p>${from} has contacted you.</p>
+      const sentMail = {
+        from: process.env.NAME + " " + process.env.USER,
+        to: process.env.NAME + " " + process.env.USER,
+        subject: subject,
+        html: `<p>${from} has contacted you.</p>
                <p>He said:</p>
                <p>${message}</p>`
-    };
+      };
 
-    const ackMail = {
-      from: process.env.NAME + " " + process.env.USER,
-      to: from,
-      subject: "Thank you for contacting",
-      html: `<p>Hey there!</p>
+      const ackMail = {
+        from: process.env.NAME + " " + process.env.USER,
+        to: from,
+        subject: "Thank you for contacting",
+        html: `<p>Hey there!</p>
              <p>I've received your email.</p>
              <p>I've reading it as soon as I could, and rest asure I'll be replying</p>
              <p>Thank you for writing</p>
              <p>Cheers</p>`
-    };
+      };
 
-    return transporter.sendMail(sentMail, (err, info) => {
-      if (err) {
-        return res.send({ success: false });
-      }
-
-      return transporter.sendMail(ackMail, (err, info) => {
+      return transporter.sendMail(sentMail, (err, info) => {
         if (err) {
           return res.send({ success: false });
         }
-        return res.send({ success: true });
+
+        return transporter.sendMail(ackMail, (err, info) => {
+          if (err) {
+            return res.send({ success: false });
+          }
+          return res.send({ success: true });
+        });
       });
     });
   });
-});
